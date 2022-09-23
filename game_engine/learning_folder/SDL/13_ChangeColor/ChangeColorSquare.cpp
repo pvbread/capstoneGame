dafc36c5f5015bb05249c1fc333f1c4eb9b3113d
acmichelman <44442437+acmichelman@users.noreply.github.com>
2022-09-23 16:48:23 -0400

    test me plz
    :)

---
+++ game_engine/learning_folder/SDL/13_ChangeColor/ChangeColorSquare.cpp
@@ -0,0 +1,46 @@
+#include <SDL.h>
+
+//draws a white canvas with a red square in the middle for 5 seconds
+int main(int argc, char* argv[])
+{
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    SDL_Event event;
+    bool quit = false;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    //SDL_RenderClear(renderer);
+
+    SDL_Rect rect = { 320, 240, 100, 100 };
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = true;
+            break;
+        }
+
+        
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+    
+}
