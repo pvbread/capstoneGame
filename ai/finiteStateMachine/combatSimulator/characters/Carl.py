070f69f086f0040c9e55cca6fa86e2344ba75ada
pvbread <64717180+pvbread@users.noreply.github.com>
2022-09-27 17:15:36 -0400

    Saving progress 2

--- ai/finiteStateMachine/combatSimulator/characters/Carl.py
+++ ai/finiteStateMachine/combatSimulator/characters/Carl.py
@@ -1,5 +1,11 @@
 from BaseCharacter import BaseCharacter
+import random
 
 class Carl(BaseCharacter):
-    def __init__(self, name, hp, speed, hitModifier, speedModifier, dodgeModifier):
-        super().__init__(name, hp, speed, hitModifier, speedModifier, dodgeModifier)
+    def __init__(self, name, hp, speed, hit, itemModifier, speedModifier, dodgeModifier):
+        super().__init__(name, hp, speed, hit, itemModifier, speedModifier, dodgeModifier)
+        self.moveSet = []
+
+    def attack(self, targetCharacter):
+        roll = random.randint(1,6) + self.hit + self.itemModifier - targetCharacter.dodgeModifier
+        if roll 
