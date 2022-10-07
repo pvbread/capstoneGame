279c1d37d64ca8b81ffb9a7d05d1eedd6bb0457a
dclin34 <80335431+dclin34@users.noreply.github.com>
2022-10-07 17:51:37 -0400

    fixed errors, account for initiative ties
    added tie conditions

--- ai/finiteStateMachine/combatSimulator/utility/setRoundTurns.py
+++ ai/finiteStateMachine/combatSimulator/utility/setRoundTurns.py
@@ -13,12 +13,24 @@ def setRoundTurns(characters):
     provisionalOrder = PriorityQueue()
     for character in characters:
         totalSpeedScore = character.speed + character.speedModifier + random.randint(1,6)
-        print(f"{character.name} got a speed score of {totalSpeedScore}")
-        if totalSpeedScore not in speedMap:
-            speedMap[-totalSpeedScore] = [character] 
+        print(f"{character.name} got a initiative of {totalSpeedScore} and a speed stat of {character.speed + character.speedModifier}")
+        if -totalSpeedScore not in speedMap:
+            speedMap[-totalSpeedScore] = [character]
         else:
             #later have to account for player characters
-            speedMap[totalSpeedScore].append(character)
+            speedMap[-totalSpeedScore].append(character)
+            # in case the initiaive is a tie. higher speed stat goes first. 
+            for i in range(len(speedMap[-totalSpeedScore])):
+                for j in range(len(speedMap[-totalSpeedScore]) - i - 1):
+                    if (speedMap[-totalSpeedScore][j].speed + speedMap[-totalSpeedScore][j].speedModifier) > (speedMap[-totalSpeedScore][j+1].speed + speedMap[-totalSpeedScore][j+1].speedModifier):
+                        speedMap[-totalSpeedScore][j], speedMap[-totalSpeedScore][j+1] = speedMap[-totalSpeedScore][j+1], speedMap[-totalSpeedScore][j]
+                    # if speed is the same. lower health character goes first
+                    elif (speedMap[-totalSpeedScore][j].speed + speedMap[-totalSpeedScore][j].speedModifier) == (speedMap[-totalSpeedScore][j+1].speed + speedMap[-totalSpeedScore][j+1].speedModifier):
+                        if speedMap[-totalSpeedScore][j].hp < speedMap[-totalSpeedScore][j+1].hp:
+                            speedMap[-totalSpeedScore][j], speedMap[-totalSpeedScore][j+1] = speedMap[-totalSpeedScore][j+1], speedMap[-totalSpeedScore][j]
+                    
+                
+            
         #python doesn't provide a max heap or maxpq, so this is a pattern to do so
         provisionalOrder.put(-totalSpeedScore, totalSpeedScore)
     while not provisionalOrder.empty():
