224a8299e2f2d2ccb5a1e85ec046ef3b87253654
pvbread <64717180+pvbread@users.noreply.github.com>
2022-10-23 15:03:18 -0400

    Added python Class test

--- game_engine/learning_folder/boost/helloFunction2/multiply.py
+++ game_engine/learning_folder/boost/helloFunction2/multiply.py
@@ -1,8 +1,8 @@
-def multiply(a,b):
+def mult(a,b):
     print("Will compute", a, "times", b)
     c = 0
     for i in range(0, a):
         c = c + b
     return c
 
-print(multiply(2,3))
+#print(multiply(2,3))
--- game_engine/learning_folder/boost/import2/helloImport.cpp
+++ game_engine/learning_folder/boost/import2/helloImport.cpp
@@ -8,29 +8,27 @@ int main(int argc, char const *argv[])
     Py_Initialize();
     PyObject* sysmodule = PyImport_ImportModule("sys");
     PyObject* syspath = PyObject_GetAttrString(sysmodule, "path");
-    PyList_Append(syspath, PyUnicode_FromString("."));
+    PyList_Append(syspath, PyUnicode_FromString("../"));
     PyObject* pName = PyUnicode_DecodeFSDefault("multiply");
     PyObject* pModule = PyImport_Import(pName);
-    /*
     PyObject* pName2 = PyUnicode_DecodeFSDefault("bar");
     PyObject* pModule2 = PyImport_Import(pName2);
+    if (!pModule)
+        std::cout << "Error loading mod" << std::endl;
     if (!pModule2)
         std::cout << "Error loading mod2" << std::endl;
-    */
+   
     PyObject* pArgs = PyTuple_New(2);
     PyObject* arg1 = PyLong_FromLong(3);
     PyObject* arg2 = PyLong_FromLong(4);
     PyTuple_SetItem(pArgs, 0, arg1);
     PyTuple_SetItem(pArgs, 1, arg2);
 
-    //std:: cout << PyObject_HasAttrString(pModule2, "foo") << std::endl;
-    
-    PyObject* pFunc = PyObject_GetAttrString(pModule, "multiply");
+    std:: cout << PyObject_HasAttrString(pModule2, "foo") << std::endl;
+    PyObject* pFunc = PyObject_GetAttrString(pModule, "mult");
 
     PyObject* pVal = PyObject_CallObject(pFunc, pArgs);
     std::cout << PyLong_AsLong(pVal);
-    
-    
 
     
     
