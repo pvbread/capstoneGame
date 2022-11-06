7142d8486044295f0f8e590aebccef8b905d3dbe
pvbread <64717180+pvbread@users.noreply.github.com>
2022-11-06 14:52:31 -0500

    Add template for multiple Tests

--- PyToCMigration/test/BasePlayerTest.cpp
+++ PyToCMigration/test/BasePlayerTest.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
+#ifndef CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_MAIN
+#endif
 #include "catch.hpp"
 
 #include "Characters/Players/BasePlayer.h"
 #include "Characters/BaseCharacter.h"
 #include <vector>
+
 
 TEST_CASE("Test attack method")
 {
