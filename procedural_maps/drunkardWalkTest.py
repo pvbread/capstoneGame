53553d34614a10e57fdb58467faf0d4e8589ecb3
dclin34 <80335431+dclin34@users.noreply.github.com>
2022-11-08 13:54:17 -0500

    map conversion draft
    cleaned code and edited conditionals (will explain the logic in more detail later)

--- procedural_maps/drunkardWalkTest.py
+++ procedural_maps/drunkardWalkTest.py
@@ -107,56 +107,155 @@ def branchMaker(x, y, length, positions_visited):
             
     return x_branch, y_branch, positions_visited
 
+"""
+encodes the martix to values that correlate to a enum tile type (does not include t-shaped tiles and 4-way tiles)
 
-# create matrix from coordinates and save matrix in text file
-def createMatrix(coordinates):
-    # create matrix filled with zeros
-    matrix = np.full((20,20), 3)
+enum TileType
+{
+    TRIGHT = 0,
+    TLEFT = 1,
+    UPRIGHT = 2,
+    BLACK = 3,
+    TUP = 4,
+    UPDOWN = 5,
+    DOWNRIGHT = 6,
+    TDOWN = 7,
+    LEFTRIGHT = 8,
+    LEFTUP = 9,
+    LEFTDOWN = 10,
+    FOURWAY = 11
+};
+"""
+def encodePath(matrix, path):
 
-    # itterate through coordinates (i = x-cord, j = y-cord) 
-    for i, j in coordinates:
-        matrix[i][j] = 1
-    for row in range(20):
-        for col in range(20):
-            if matrix[row][col] != 3:
-                if matrix[row+1][col] != 3 and matrix[row-1][col] != 3 and matrix[row][col+1] != 3 and matrix [row][col-1] != 3:
-                    matrix[row][col] = 11
-                elif matrix[row+1][col] != 3 and matrix[row-1][col] == 3 and matrix[row][col+1] == 3 and matrix [row][col-1] != 3:
-                    matrix[row][col] = 10
-                elif matrix[row+1][col] == 3 and matrix[row-1][col] != 3 and matrix[row][col+1] == 3 and matrix [row][col-1] != 3:
-                    matrix[row][col] = 9
-                elif matrix[row+1][col] == 3 and matrix[row-1][col] == 3 and matrix[row][col+1] != 3 and matrix [row][col-1] != 3:
-                    matrix[row][col] = 8
-                elif matrix[row+1][col] not in [7,3] and matrix[row-1][col] == 3 and matrix[row][col+1] not in [7,3] and matrix [row][col-1] not in [7,3]:
-                    matrix[row][col] = 7
-                elif matrix[row+1][col] != 3 and matrix[row-1][col] == 3 and matrix[row][col+1] != 3 and matrix [row][col-1] == 3:
-                    matrix[row][col] = 6
-                elif matrix[row+1][col] != 3 and matrix[row-1][col] != 3 and matrix[row][col+1] == 3 and matrix [row][col-1] == 3:
-                    matrix[row][col] = 5 
-                elif matrix[row+1][col] == 3 and matrix[row-1][col] not in [4,3] and matrix[row][col+1] not in [4,3] and matrix [row][col-1] not in [4,3]:
-                    matrix[row][col] = 4
-                elif matrix[row+1][col] == 3 and matrix[row-1][col] != 3 and matrix[row][col+1] != 3 and matrix [row][col-1] == 3:
-                    matrix[row][col] = 2
-                elif matrix[row+1][col] not in [1,3] and matrix[row-1][col] not in [1,3] and matrix[row][col+1] == 3 and matrix [row][col-1] not in [1,3]:
-                    matrix[row][col] = 1
-                elif matrix[row+1][col] not in [0,3] and matrix[row-1][col] not in [0,3] and matrix[row][col+1] not in [0,3] and matrix [row][col-1] == 3:
-                    matrix[row][col] = 0
-            else:
-                matrix[row][col] = 3
+    # variables that start with 'r' represents the row, variables that start with 'c' represents the col
+
+    # itterate through the path list and encode the path with the proper tile type within the matrix
+    for i in range(len(path)-2):
         
-    #     TRIGHT = 0,
-    #     TLEFT = 1,
-    #     UPRIGHT = 2,
-    #     BLACK = 3,
-    #     TUP = 4,
-    #     UPDOWN = 5,
-    #     DOWNRIGHT = 6,
-    #     TDOWN = 7,
-    #     LEFTRIGHT = 8,
-    #     LEFTUP = 9,
-    #     LEFTDOWN = 10,
-    #     FOURWAY = 11
-    # save matrix in textfile
+        """
+        We needed to keep track of 3 coordinates to determine the tile shape and direction
+        since we know what the tile shape and direction of the first coordinate of the path, we can ignore and skip to the next coordinate
+        """
+        
+        # these variables will represent the previous coordinates
+        rFirst, cFirst = path[i]
+        # these variables will represent the current coordinates
+        rMid, cMid = path[i+1]
+        # these variables will represent the next coordinates
+        rLast, cLast = path[i+2]
+
+        #Switch case to detemine the tile type: the AND conditions helps deterine the tile shape, the OR conditions determine the direction of the path
+        
+        # Case: LEFTRIGHT
+        if (rFirst == rMid and rLast == rMid and cFirst + 1 == cMid and cLast - 1 == cMid) or (rFirst == rMid and rLast == rMid and cFirst - 1 == cMid and cLast + 1 == cMid):
+            matrix[rMid][cMid] = 8
+        # Case: UPDOWN
+        elif (rFirst + 1 == rMid and rLast - 1 == rMid and cFirst == cMid and cLast == cMid) or (rFirst - 1 == rMid and rLast + 1 == rMid and cFirst == cMid and cLast == cMid):
+            matrix[rMid][cMid] = 5
+        # Case: DOWNRIGHT
+        elif (rFirst + 1 == rMid and rLast == rMid and cFirst == cMid and cLast - 1 == cMid) or (rFirst == rMid and rLast + 1 == rMid and cFirst - 1 == cMid and cLast == cMid):
+            matrix[rMid][cMid] = 6
+        # Case: LEFTDOWN
+        elif(rFirst == rMid and rLast - 1 == rMid and cFirst + 1 == cMid and cLast == cMid) or (rFirst - 1 == rMid and rLast == rMid and cFirst == cMid and cLast + 1 == cMid):
+            matrix[rMid][cMid] = 10
+        # Case: LEFTUP  
+        elif(rFirst == rMid and rLast + 1 == rMid and cFirst + 1 == cMid and cLast == cMid) or (rFirst + 1 == rMid and rLast == rMid and cFirst == cMid and cLast + 1 == cMid):
+            matrix[rMid][cMid] = 9
+        # Case: UPRIGHT
+        elif(rFirst - 1 == rMid and rLast == rMid and cFirst == cMid and cLast - 1 == cMid) or (rFirst  == rMid and rLast - 1 == rMid and cFirst - 1 == cMid and cLast == cMid):
+            matrix[rMid][cMid] = 2
+        # the number 12 will represent the end point of the path
+        matrix[rLast][cLast] = 12
+
+    # return an updated version of the matrix
+    return matrix
+
+"""
+creates a matrix from coordinates of the three paths and save matrix in text file (x -> row, y -> col)
+"""
+def createMatrix(path1, path2, path3):
+    
+    # create 30x30 matrix filled with threes
+    matrix = np.full((30,30),3)
+
+    # variables that start with 'r' represents the row, variables that start with 'c' represents the col
+
+    """
+    encode first path
+    """
+    
+    # origin point will only have the UPDOWN tile or the LEFTRIGHT tile (the origin will always start at (0,0): the beginning of the matrix)
+    rStart, cStart = path1[0]
+    # need to see the next step to figure out the direction the origin tile should be
+    rStep, cStep = path1[1]
+
+    # if horizontal line, LEFTRIGHT tile
+    if rStart == rStep and cStart + 1 == cStep: 
+        matrix[rStart][cStart] = 8
+    # if vertical line, UPDOWN tile
+    elif rStart + 1 == rStep and cStart == cStep:
+        matrix[rStart][cStart] = 5
+
+    # calls function to encode the first path
+    matrix = encodePath(matrix, path1)
+
+    """
+    encode second path
+    """
+
+    # the first coordinate of the branching paths always start with a point somewhere in the middle of the first path
+    rMidpoint, cMidpoint = path2[0]
+    # need to see the next step to figure out what T-shaped tile should be replaced for the branching path (T-shaped tiles only used to add a branching path)
+    rStep, cStep = path2[1]
+
+    # Case: TDOWN
+    if rMidpoint + 1 == rStep and cMidpoint == cStep:
+        matrix[rMidpoint][cMidpoint] = 7
+    # Case: TUP
+    elif rMidpoint - 1 == rStep and cMidpoint == cStep:
+        matrix[rMidpoint][cMidpoint] = 4
+    # Case: TRIGHT
+    elif rMidpoint == rStep and cMidpoint + 1 == cStep:
+        matrix[rMidpoint][cMidpoint] = 0
+    # Case: TLEFT
+    elif rMidpoint == rStep and cMidpoint - 1 == cStep:
+        matrix[rMidpoint][cMidpoint] = 1
+        
+    # calls function to encode the second path
+    matrix = encodePath(matrix, path2)
+
+    """
+    encode third path
+    """
+
+    # now find the start of the third path which will also start somewhere in the middle of the first path
+    rMidpoint, cMidpoint = path3[0]
+    # need to see the next step to figure out if either a T-shaped tile or a FOURWAY tile is needed to represent branching path in the matrix
+    rStep, cStep = path3[1]
+
+    # if the second path and the third path has the same starting point, replace the point with a FOURWAY tile
+    if path2[0] == path3[0]:
+        matrix[rMidpoint][cMidpoint] = 11
+    # else replace with a T-shaped tile
+    else:
+        # Case: TDOWN
+        if rMidpoint + 1 == rStep and cMidpoint == cStep:
+            matrix[rMidpoint][cMidpoint] = 7
+        # Case: TUP
+        elif rMidpoint - 1 == rStep and cMidpoint == cStep:
+            matrix[rMidpoint][cMidpoint] = 4
+        # Case: TRIGHT
+        elif rMidpoint == rStep and cMidpoint + 1 == cStep:
+            matrix[rMidpoint][cMidpoint] = 0
+        # Case: TLEFT
+        elif rMidpoint == rStep and cMidpoint - 1 == cStep:
+            matrix[rMidpoint][cMidpoint] = 1
+
+    # calls function to encode the third path
+    matrix = encodePath(matrix, path3)
+
+    # save matrix in textfile, format as whole number digits
     np.savetxt('output.txt', matrix, fmt='%d')
    
 
@@ -164,35 +263,50 @@ def createMatrix(coordinates):
 Draw the paths with visualization using matplotlib
 """
 def plotWalk(path_length):
-    # draw the first self avoiding walk path
-    x, y, positions_visited = pathMaker(path_length)
+    # draw the first self avoiding walk path (the original first path)
+    x1, y1, positions_visited = pathMaker(path_length)
     plt.figure(figsize = (8, 8))
-    plt.plot(x, y, 'bo-', linewidth = 1)
-    plt.plot(x[0], y[0], 'go', ms = 12, label = 'Start')
-    plt.plot(x[-1], y[-1], 'ro', ms = 12, label = 'End')
+    plt.plot(x1, y1, 'bo-', linewidth = 1)
+    plt.plot(x1[0], y1[0], 'go', ms = 12, label = 'Start')
+    plt.plot(x1[-1], y1[-1], 'ro', ms = 12, label = 'End')
     
     # store the actual length of inital path since the path might not always be the desired length due to being stuck 
-    new_length = len(x)
+    new_length = len(x1)
     
-    # draw two self avoiding walk that branches in different directions at halfway point of the first initial path
-    for i in range(2):
-        x_branch, y_branch, positions_visited = branchMaker(x, y, new_length, positions_visited)
-        plt.plot(x_branch, y_branch, 'bo-', linewidth = 1)
-        plt.plot(x_branch[-1], y_branch[-1], 'ro', ms = 12)
-        # append the branches to the x and y coordinates list that will be used to convert to matrix (not including duplicate of midpoint value)   
-        x = x + x_branch[1:]
-        y = y + y_branch[1:]
+    # draw a branch somewhere on the original path (the second path)
+    x2, y2, positions_visited = branchMaker(x1, y1, new_length, positions_visited)
+    plt.plot(x2, y2, 'bo-', linewidth = 1)
+    plt.plot(x2[-1], y2[-1], 'ro', ms = 12)
+    
+    # draw another branch somewhere on the original path (the third path)
+    x3, y3, positions_visited = branchMaker(x1, y1, new_length, positions_visited)
+    plt.plot(x3, y3, 'bo-', linewidth = 1)
+    plt.plot(x3[-1], y3[-1], 'ro', ms = 12)
+    
     plt.axis('equal')
     plt.legend()
     plt.savefig("map.png")
     plt.show()
-    # coordinates is the list of array of the (x,y) coordinates
-    coordinates=[]
-    # append coordinates to the list
-    for i in range(len(x)):
-        coordinates.append((x[i],y[i]))
+    
+    # these variables will store a list of array of the (x,y) coordinates of the 3 branching paths in order to draw the matrix
+    path1 = []
+    path2 = []
+    path3 = []
+    
+    # append coordinates to its corresponding path list
+
+    # first initial path
+    for i in range(len(x1)):
+        path1.append((x1[i], y1[i]))
+    # the second path
+    for i in range(len(x2)):
+        path2.append((x2[i], y2[i]))
+    # the third path
+    for i in range(len(x3)):
+        path3.append((x3[i], y3[i]))
+    
     # call function to create matrix with coordinates and save as textfile
-    createMatrix(coordinates)
+    createMatrix(path1, path2, path3)
     
 
 """
