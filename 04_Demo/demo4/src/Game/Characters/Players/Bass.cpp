28d275cf0b4f955825daae36e5cc319863c3d8ca
pvbread <64717180+pvbread@users.noreply.github.com>
2022-12-11 16:10:17 -0500

    Revert "Merge pull request #83 from pvbread/FINAL-DEMOS-FOR-REAL"
    This reverts commit 960b10db13e5b1cc8c073a45be278f0381f10f4e, reversing
    changes made to 96d55b790f2b7bef95df4e1e8f598e35d43d38b2.

--- 04_Demo/demo4/src/Game/Characters/Players/Bass.cpp
+++
@@ -1,8 +0,0 @@
-#include "Game/Characters/Players/Bass.h"
-
-Bass::Bass(std::string name, int hp, int speed, 
-                             int hit, int armor, int itemModifier, 
-                             int speedModifier, int dodgeModifier) :
-                                    BasePlayer(name, hp, speed, hit, armor, 
-                                               itemModifier, speedModifier, 
-                                               dodgeModifier) {}
